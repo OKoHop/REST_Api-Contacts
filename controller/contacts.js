391f3658bc70ed80c575f6044d8040c7b6ca881e
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-11-11 17:45:19 +0200

    upd fun ctrl

--- controller/contacts.js
+++ controller/contacts.js
@@ -1,41 +1,47 @@
 const { Contact } = require("../models/contact");
 const { ctrlWrap } = require("../helpers");
 
-const getAll = async (req, res) => {
+const getAll = async (req, res, _) => {
   const data = await Contact.find();
   res.status(200).send(data);
 };
 
-const getById = async (req, res) => {
+const getById = async (req, res, _) => {
   const { contactId } = req.params;
   const data = await Contact.findById(contactId);
   res.status(200).send(data);
 };
 
-const addContact = async (req, res) => {
+const addContact = async (req, res, _) => {
   const data = await Contact.create(req.body);
   res.status(201).send(data);
 };
 
-const updateContact = async (req, res) => {
+const updateContact = async (req, res, _) => {
   const updateContact = req.body;
   const { contactId } = req.params;
   const data = await Contact.findByIdAndUpdate(contactId, updateContact, {
     new: true,
   });
+  if (!data) {
+    res.status(404).send({ message: "Not found" });
+  }
   res.status(200).send(data);
 };
 
-const patchContact = async (req, res) => {
+const patchContact = async (req, res, _) => {
   const { contactId } = req.params;
   const updateFavorite = req.body;
   const data = await Contact.findByIdAndUpdate(contactId, updateFavorite, {
     new: true,
   });
+  if (!data) {
+    res.status(404).send({ message: "Not found" });
+  }
   res.status(200).send(data);
 };
 
-const deleteContact = async (req, res) => {
+const deleteContact = async (req, res, _) => {
   const { contactId } = req.params;
   const data = await Contact.findByIdAndDelete(contactId);
   res.status(200).send(data);
